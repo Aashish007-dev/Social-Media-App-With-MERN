661c75e4ea33183e4d07cf9d163ee07181fd2ae9
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-04-16 11:02:31 +0530

    feat: implement StoriesBar component and integrate it into the Feed page

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets';
+import Loading from '../components/Loading';
+import StoriesBar from '../components/StoriesBar';
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  const fetchFeed = async () => {
+ 
+    setFeeds(dummyPostsData)
+    setLoading(false)
+  
+  }
+
+  useEffect(() => {
+    fetchFeed()
+  }, []);
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* Stories and Post list */}
+
+      <div>
+          <StoriesBar />
+          <div className='pt-4 space-y-6'>
+            List of Post
+          </div>
+      </div>
+
+      {/* Right side bar */}
+      <div>
+        <div>
+          <h1>Sponsored</h1>
+        </div>
+        <h1>Recent messages</h1>
+      </div>
+
+    </div>
+  ) : (
+    <Loading />
   )
 }
 
